7451fbcb261dea62772d1657cbfeb5a21d68291e
SrGobi <alejandrosanjuangallegos@gmail.com>
2024-06-29 16:28:09 +0200

    ♻️ Update tailwind.config.mjs to include flowbite plugin and additional content patterns

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
+	darkMode: 'media',
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('flowbite/plugin')
+	],
 }
