6afd261f771ec8e3391653d867648cdb4cd09cfd
SrGobi <alejandrosanjuangallegos@gmail.com>
2024-06-30 20:37:15 +0200

    ♻️ GitHubPages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://dev.srgobi.com'
+  site: 'https://srgobi.com'
 })
