4837dde9d06594957d34861d1871f0de499685db
SrGobi <alejandrosanjuangallegos@gmail.com>
2024-07-29 12:51:31 +0200

    ♻️ Update Astro site configuration, remove unused files, and optimize page load

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://srgobi.github.io/portfolio.srgobi'
+  site: 'https://srgobi.com'
 })
